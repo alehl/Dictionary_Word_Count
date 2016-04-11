4f8d77729fcbc8d17b7b197eca6bb3ebbffe7c5f
Rosie <rosie@riveter16-md.swna.wdpr.disney.com>
2016-04-11 12:56:56 -0700

    Made the dictionary super fast by using the get method

--- wordcount.py
+++ wordcount.py
@@ -5,17 +5,12 @@ def word_count(file):
 
 	for line in poem_file:
 		line = line.rstrip()
-		word_list = line.split(" ") # This separates the line into a list of strings
-		for word in word_list:
-			if word in ives_dictionary:
-				ives_dictionary[word] += 1
-			else:
-				ives_dictionary[word] = 1
+		word_list = line.split(" ")
+			ives_dictionary[word] = ives_dictionary.get(word, 0) + 1
+
 	for word, quantity in ives_dictionary.iteritems():
 		quantity = ives_dictionary[word]
 		print word, quantity
-
-
 
 
 word_count("test.txt")
